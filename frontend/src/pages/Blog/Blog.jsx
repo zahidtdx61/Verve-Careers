7c1b819c9a3da2bd427f076e060bcc8ae9932061
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-11-21 19:36:41 +0600

    added: blog page

---
+++ frontend/src/pages/Blog/Blog.jsx
@@ -0,0 +1,62 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Markdown from "react-markdown";
+import { useParams } from "react-router-dom";
+import rehypeRaw from "rehype-raw";
+import placeholderImg from "/404.jpg";
+
+const Blog = () => {
+  const { id } = useParams();
+  const { data: blog, isLoading: blogLoading } = useQuery({
+    queryKey: ["blog", id],
+    queryFn: async () => {
+      const response = await axios(`https://dev.to/api/articles/${id}`);
+      return response.data;
+    },
+  });
+
+  if (blogLoading) {
+    return <p>Loading...</p>;
+  }
+
+  const { cover_image, title, tags, url, body_html } = blog;
+
+  return (
+    <div className="mx-auto max-w-screen-xl transition p-2  border-opacity-30   group hover:no-underline focus:no-underline ">
+      <img
+        className="object-contain w-full rounded h-56 "
+        src={cover_image || placeholderImg}
+      />
+      <div>
+        <div className="flex flex-wrap py-6 gap-2 border-t border-dashed ">
+          {tags &&
+            tags.map((tag) => (
+              <a
+                key={tag}
+                rel="noopener noreferrer"
+                href="#"
+                className="px-3 py-1 rounded-sm hover:underline "
+              >
+                #{tag}
+              </a>
+            ))}
+        </div>
+      </div>
+      <div className="space-y-2">
+        <a
+          target="_blank"
+          href={url}
+          className="text-2xl font-semibold group-hover:underline group-focus:underline"
+        >
+          {title}
+        </a>
+
+        <div className="mx-auto w-fit">
+          <Markdown rehypePlugins={[rehypeRaw]}>{body_html}</Markdown>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,6 +1,9 @@
-import AllJobs from "@/pages/AllJobs/AllJobs";
+import Job from "@/pages/Job/Job";
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout/MainLayout";
+import AllJobs from "../pages/AllJobs/AllJobs";
+import Blog from "../pages/Blog/Blog";
+import Blogs from "../pages/Blogs/Blogs";
 import Home from "../pages/Home/Home";
 import Registration from "../pages/Registration/Registration";
 
@@ -20,6 +23,18 @@ const router = createBrowserRouter([
       {
         path: "all-jobs",
         element: <AllJobs />,
+      },
+      {
+        path: "blogs",
+        element: <Blogs />,
+      },
+      {
+        path: "blog/:id",
+        element: <Blog />,
+      },
+      {
+        path: "job/:id",
+        element: <Job />,
       },
     ],
   },
